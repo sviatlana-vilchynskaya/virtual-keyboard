57aa9b09be427fd24041ea5645c96ca45fedfb73
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2023-04-25 20:20:26 +0300

    feat: added keyboard display on screen and mouse click handling on buttons
    Added display of keyboard on screen when physical keys are pressed. Mouse click handling on virtual keyboard buttons is now also supported. Text output is now performed in a textarea.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './assets/styles/main.scss';
+import keyData from './assets/scripts/keyData.js';
 
 class Keyboard {
   constructor() {
@@ -11,11 +12,13 @@ class Keyboard {
     this.title = document.createElement('h1');
     this.textarea = document.createElement('textarea');
     this.keyboard = document.createElement('section');
+    this.key = document.createElement('button');
     this.description = document.createElement('p');
 
     this.main.classList.add('main', 'container');
     this.title.classList.add('main__title');
     this.textarea.classList.add('main__textarea');
+    this.textarea.focus();
     this.keyboard.classList.add('keyboard');
     this.description.classList.add('keyboard__description');
 
@@ -30,10 +33,40 @@ class Keyboard {
     this.keyboard.appendChild(this.description);
 
     document.body.appendChild(this.main);
+
+    keyData.forEach((row) => {
+      const rowElement = document.createElement('div');
+      rowElement.classList.add('keyboard__row');
+      row.forEach((buttonData) => {
+        const buttonElement = document.createElement('button');
+        buttonElement.classList.add('keyboard__button');
+        buttonElement.setAttribute('type', 'button');
+        buttonElement.setAttribute('data-code', buttonData.code);
+        buttonElement.setAttribute('data-lang-en', buttonData.lang.en);
+        buttonElement.setAttribute('data-lang-ru', buttonData.lang.ru);
+        buttonElement.setAttribute('func', buttonData.func);
+        buttonElement.classList.add(`keyboard__button-${buttonData.width}`);
+        buttonElement.textContent = buttonData.lang.en;
+        rowElement.appendChild(buttonElement);
+      });
+      this.keyboard.appendChild(rowElement);
+    });
+
+    this.keyboard.addEventListener('click', (event) => {
+      const button = event.target;
+
+      if (button.tagName === 'BUTTON') {
+        const value = button.getAttribute('data-lang-en');
+        const funcBoolean = button.getAttribute('func');
+        if (funcBoolean === 'false') {
+          this.textarea.value += value;
+        }
+      }
+    });
   }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  const keyboard = new Keyboard();
-  keyboard.init();
+  const virtualKeyboard = new Keyboard();
+  virtualKeyboard.init();
 });
